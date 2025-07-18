0170cf92adfee51cdd34169f42c97301b056c641
birdhimself <git@wuul.eu>
2025-07-18 16:51:45 +0200

    Don't separate box64 install into stage

---
+++ proton/install-box64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$TARGETARCH" = "arm64" ]; then
+  cat <<EOF > /etc/apt/sources.list.d/box64.sources
+Types: deb
+URIs: https://Pi-Apps-Coders.github.io/box64-debs/debian
+Suites: ./
+Signed-By: /etc/apt/keyrings/box64-archive-keyring.gpg
+EOF
+  wget -qO- "https://pi-apps-coders.github.io/box64-debs/KEY.gpg" | gpg --dearmor -o /etc/apt/keyrings/box64-archive-keyring.gpg
+  apt-get update
+  apt-get install -y box64-generic-arm
+fi
