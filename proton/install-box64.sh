ba69e9c31288f471a6521371a03398e51f1ebb04
birdhimself <git@wuul.eu>
2025-07-19 11:55:18 +0200

    Fix box64 not being installed

--- proton/install-box64.sh
+++ proton/install-box64.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-if [ "$TARGETARCH" = "arm64" ]; then
-  cat <<EOF > /etc/apt/sources.list.d/box64.sources
+cat <<EOF > /etc/apt/sources.list.d/box64.sources
 Types: deb
 URIs: https://Pi-Apps-Coders.github.io/box64-debs/debian
 Suites: ./
 Signed-By: /etc/apt/keyrings/box64-archive-keyring.gpg
 EOF
-  wget -qO- "https://pi-apps-coders.github.io/box64-debs/KEY.gpg" | gpg --dearmor -o /etc/apt/keyrings/box64-archive-keyring.gpg
-  apt-get update
-  apt-get install -y box64-generic-arm
-fi
+wget -qO- "https://pi-apps-coders.github.io/box64-debs/KEY.gpg" | gpg --dearmor -o /etc/apt/keyrings/box64-archive-keyring.gpg
+apt-get update
+apt-get install -y box64-generic-arm
