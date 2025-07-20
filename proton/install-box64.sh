565e3f28c908762fed836e91b387854835e54bc9
birdhimself <git@wuul.eu>
2025-07-20 16:11:40 +0200

    Revert using box64-wrapper
    Works in theory, however when a wrapped process calls itself in certain
    ways (like Wine seems to do in some cases, e.g. `wine64 wineboot`), it
    then attempts to run unwrapped wine64 which doesn't work.

--- proton/install-box64.sh
+++ proton/install-box64.sh
@@ -9,10 +9,3 @@ EOF
 wget -qO- "https://pi-apps-coders.github.io/box64-debs/KEY.gpg" | gpg --dearmor -o /etc/apt/keyrings/box64-archive-keyring.gpg
 apt-get update
 apt-get install -y box64-generic-arm
-
-cd /opt/proton/files/bin/
-
-for bin in wine64 wine64-preloader wineserver; do
-  mv "$bin" "${bin}.amd64"
-  ln -sf /opt/wrappers/box64-wrapper "$bin"
-done
